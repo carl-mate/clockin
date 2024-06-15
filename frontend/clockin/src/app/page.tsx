555bf34cfa82a39e586463f69b136880af01590a
cmate <cmate61@gmail.com>
2024-06-15 22:28:28 +0800

    fix: comment in checkin list

--- frontend/clockin/src/app/page.tsx
+++ frontend/clockin/src/app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
         </Button>
         <Dashboard />
         <CheckinForm />
-        {/* <CheckinList /> */}
+        <CheckinList />
       </Container>
     </ProtectedRoute>
   );
