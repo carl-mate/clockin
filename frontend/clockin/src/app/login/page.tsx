ded0e5a7f538b92e87c5f133377d90dc0163a08d
cmate <cmate61@gmail.com>
2024-06-15 15:55:06 +0800

    feat: add links under login/register forms

--- frontend/clockin/src/app/login/page.tsx
+++ frontend/clockin/src/app/login/page.tsx
@@ -1,5 +1,20 @@
+import { Container, Typography, Button } from '@mui/material';
+import Link from 'next/link';
+
 import LoginForm from '../components/LoginForm';
 
 export default function LoginPage() {
-  return <LoginForm />;
+  return (
+    <Container maxWidth='lg'>
+      <LoginForm />
+      <Typography align='center' mt={2}>
+        Don{"'"}t have an account?{' '}
+        <Link href='/register' passHref>
+          <Button variant='text' color='primary'>
+            Register
+          </Button>
+        </Link>
+      </Typography>
+    </Container>
+  );
 }
--- frontend/clockin/src/app/register/page.tsx
+++ frontend/clockin/src/app/register/page.tsx
@@ -1,5 +1,20 @@
+import { Container, Typography, Button } from '@mui/material';
+import Link from 'next/link';
+
 import RegisterForm from '../components/RegisterForm';
 
 export default function RegisterPage() {
-  return <RegisterForm />;
+  return (
+    <Container maxWidth='lg'>
+      <RegisterForm />
+      <Typography align='center' mt={2}>
+        Already have an account?{' '}
+        <Link href='/login' passHref>
+          <Button variant='text' color='primary'>
+            Login
+          </Button>
+        </Link>
+      </Typography>
+    </Container>
+  );
 }
