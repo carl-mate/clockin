51c2658740e2b0323902ac084ce9ecc9853fe5ea
cmate <cmate61@gmail.com>
2024-06-15 19:24:34 +0800

    fix: remove unnecessary functions

--- frontend/clockin/src/app/components/CheckinList/hooks.ts
+++ frontend/clockin/src/app/components/CheckinList/hooks.ts
@@ -1,19 +1,8 @@
 import { useState, useEffect } from "react";
 import axiosInstance from "@/app/utils/axios";
 
-import fetchUser, { type User } from "@/app/utils/fetchUser";
-
 export function useHooks() {
-  const [user, setUser] = useState<User | null>(null);
   const [checkins, setCheckins] = useState<any[]>([]);
-
-  useEffect(() => {
-    const getCurrentUser = async () => {
-      const currentUser = await fetchUser();
-      setUser(currentUser);
-    }
-    getCurrentUser();
-  }, []);
 
   useEffect(() => {
     const fetchCheckins = async () => {
