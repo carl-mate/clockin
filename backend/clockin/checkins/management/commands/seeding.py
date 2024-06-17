11447439120caad436673e58ec8b75fce8b124fb
cmate <cmate61@gmail.com>
2024-06-17 18:24:14 +0800

    nit: remove unnecessary code

--- backend/clockin/checkins/management/commands/seeding.py
+++ backend/clockin/checkins/management/commands/seeding.py
@@ -1,38 +1,3 @@
-# from django.contrib.auth.models import User
-# from django.core.exceptions import ValidationError
-# from django_seed import Seed
-# from checkins.models import Checkin
-
-
-# class CheckinSeeder(Seed):
-
-#     def run(self, *args, **kwargs):
-#         self.seeder = Seed.seeder()
-#         # Existing primary key you want to seed data for
-#         user_id = 1
-
-#         try:
-#             user = User.objects.get(id=user_id)
-#         except User.DoesNotExist:
-#             print(f"User with ID {user_id} not found. Seeding skipped.")
-#             return  # Exit the function if user doesn't exist
-
-#         # Generate data for 1000 checkins
-#         data = []
-#         for _ in range(1000):
-#             data.append({
-#                 'user': user,
-#                 'hours': self.seeder.faker.pydecimal(positive=True, max_digits=3, decimal_places=2),
-#                 'tag': self.seeder.faker.word(),
-#                 'activity': self.seeder.faker.sentence(),
-#             })
-
-#         for item in data:
-#             try:
-#                 checkin = Checkin(**item)
-#                 checkin.save()
-#             except ValidationError as e:
-#                 print(f"Error creating Checkin: {e}")
 from django.core.management.base import BaseCommand
 from django_seed import Seed
 from django.contrib.auth.models import User
