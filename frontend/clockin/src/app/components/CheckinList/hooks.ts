01bd400417a9c2d4ddab25d5595fc174f855ad83
cmate <cmate61@gmail.com>
2024-06-17 13:52:35 +0800

    nit: remove unnecessary default values

--- frontend/clockin/src/app/components/CheckinList/hooks.ts
+++ frontend/clockin/src/app/components/CheckinList/hooks.ts
@@ -12,9 +12,7 @@ type CheckIn = {
 };
 
 export function useHooks() {
-  const [checkins, setCheckins] = useState<any[]>([
-    { id: 1, hours: 5, tag: "testing", activity: "more activity" },
-  ]);
+  const [checkins, setCheckins] = useState<any[]>();
   const [user, setUser] = useState<User | null>(null);
   const [input, setInput] = useState("");
   const [error, setError] = useState<string | null>(null);
