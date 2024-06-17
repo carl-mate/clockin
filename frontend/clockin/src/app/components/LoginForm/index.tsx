f0b041c70d51db9b4a48abeb37dc3bc64a421af0
cmate <cmate61@gmail.com>
2024-06-17 14:51:25 +0800

    style: improve login and signup ui

--- frontend/clockin/src/app/components/LoginForm/index.tsx
+++ frontend/clockin/src/app/components/LoginForm/index.tsx
@@ -1,25 +1,44 @@
 "use client";
 
-import { Typography, Box, TextField, Button } from "@mui/material";
+import { Avatar, Typography, Box, TextField, Button } from "@mui/material";
+import { LockOutlined } from "@mui/icons-material";
 import useHooks from "./hooks";
 
 export default function LoginForm() {
   const { register, handleSubmit, onSubmit } = useHooks();
   return (
-    <>
-      <Typography variant="h4">Login</Typography>
+    <Box
+      sx={{
+        marginTop: 8,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+        <LockOutlined />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Login
+      </Typography>
       <Box component="form" onSubmit={handleSubmit(onSubmit)}>
         <TextField
           label="Username"
           {...register("username")}
           fullWidth
           margin="normal"
+          required
+          autoComplete="username"
+          autoFocus
         />
         <TextField
           label="Email"
           {...register("email")}
           fullWidth
           margin="normal"
+          required
+          autoComplete="username"
+          autoFocus
         />
         <TextField
           label="Password"
@@ -27,11 +46,19 @@ export default function LoginForm() {
           {...register("password")}
           fullWidth
           margin="normal"
+          required
+          autoComplete="username"
+          autoFocus
         />
-        <Button type="submit" variant="contained" color="primary">
+        <Button
+          type="submit"
+          variant="contained"
+          fullWidth
+          sx={{ mt: 3, mb: 2 }}
+        >
           Login
         </Button>
       </Box>
-    </>
+    </Box>
   );
 }
